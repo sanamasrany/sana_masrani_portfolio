b250fb5af6a03053ee346ce95421aa3458067d39
sanamasrany <sanamasrany@users.noreply.github.com>
2025-09-03 06:37:15 +0000

    deploy: 19389ac18a11ed62df6bd87ddf97e5fa7f4dcd93

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "355231618"
+    serviceWorkerVersion: "2097765669"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "262525e2081311609d1fdab966c82bfc",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "269f971cec0d5dc864fe9ae080b19e23",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "15d54d142da2f2d6f2e90ed1d55121af",
-"flutter_bootstrap.js": "4d58faf40325a8ae34cb98e5226ae755",
+"flutter_bootstrap.js": "c69bec554948f6b54124ae7df5b5e3a7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "defe0986b569e051abb4337569af576b",
 "/": "defe0986b569e051abb4337569af576b"};
